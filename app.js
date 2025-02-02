63a964041c8ee935bac1a4bf76e57a966d64b351
Thijs Kamphuis <thijskamphuis@icloud.com>
2025-02-02 18:12:42 +0100

    init

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const PORT = 727;
+const files = fs.readdirSync(__dirname);
+const zipFile = files.find(file => file.endsWith(".zip"));
+if (zipFile) {
+    zipname = path.basename(zipFile, ".zip");
+} else {
+    throw new Error("No zip file found in the directory");
+}
+
+const zipFilePath = path.join(__dirname, zipname + ".zip");
+
+app.get("/download", (req, res) => {
+    res.download(zipFilePath, `${zipname}.zip`, (err) => {
+        if (err) {
+            console.error("Error sending file:", err);
+            res.status(500).send("Error downloading file");
+        }
+        console.log(req.ip);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
